b2fef81a4f15ffcf8b1daef03915b312b4a220cf
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-11-05 20:27:40 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,10 +4,6 @@ import { Heading } from '@chakra-ui/react';
 
 
 
-
-
-
-
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
 import { VStack, IconButton } from '@chakra-ui/react';
